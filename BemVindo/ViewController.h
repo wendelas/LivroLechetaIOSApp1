c52d63dc0d8a3191d0d16249af761a340bdbb4cb
wendelas <wendelas@gmail.com>
2016-08-26 21:44:47 -0300

    Initial Commit

---
+++ BemVindo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BemVindo
+//
+//  Created by Wendel de Assis da Silva on 26/08/16.
+//  Copyright © 2016 Bitcom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BemVindo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BemVindo
+//
+//  Created by Wendel de Assis da Silva on 26/08/16.
+//  Copyright © 2016 Bitcom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
